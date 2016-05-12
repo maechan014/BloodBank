8193f63e496e5d784073848ecb67423be0704cd2
maharleeeka <maharlikaalberca@gmail.com>
2016-05-12 07:00:54 -0800

    stuff

--- Interface/Admin/admin-withdrawalSummary.php
+++ Interface/Admin/admin-withdrawalSummary.php
@@ -6,6 +6,13 @@ $withdrawal = pg_query($sql);
 
 ?>
 
+
+<?php
+$db = pg_connect("host=localhost port=5432 dbname=bloodbank user=postgres password=admin");
+
+   $query = "SELECT trackingno, bloodtype, bloodrh, amount, age(date) from blood where withdrawalstatus='f'";
+   $galloner=pg_query($query);
+?>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -79,9 +86,47 @@ $withdrawal = pg_query($sql);
 
 		   pg_close($db);
 		?>
-
-
 </table>
+
+
+
+	<h2>Remaining Blood</h2>
+
+		 <table width="600" border="2" cellspacing="3" cellpadding="3">
+		      <tr>
+		         <th>Tracking No</th>
+		         <th>Blood Type</th>
+		         <th>Blood RH</th>
+		         <th>Amount</th>
+		         <th>Time Span</th>
+		      </tr>
+		   
+		<!-- Displays the request table together with a button on each record-->
+		<?php
+		   if(!$galloner){
+		      echo pg_last_error($db);
+		      exit;
+		   }
+		   $i=1;
+
+		   echo "<form action='approve123.php' method='post'>";
+		   while($records = pg_fetch_array($galloner)){
+		      echo "<tr>";
+		      echo "<td>" . $records['trackingno'] . "</td>";
+		      echo "<td>" . $records['bloodtype'] . "</td>";
+		      echo "<td>" . $records['bloodrh'] . "</td>";
+		      echo "<td>" . $records['amount'] . "</td>";
+		      echo "<td>" . $records['age'] . "</td>";
+		      echo "<br>";
+		      $i++;
+		   }
+
+		   pg_close($db);
+		?>
+
+
+		</table>
+
 
 
 </div>
