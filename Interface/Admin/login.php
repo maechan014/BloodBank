f8007cbd8b0fbdc7a4d79aa5407cfdc1f042e7bf
jessduran <duran.jessahmarie@gmail.com>
2016-05-22 16:58:52 +0800

    admin login

---
+++ Interface/Admin/login.php
@@ -0,0 +1,27 @@
+<?php 
+	$db = pg_connect("host=localhost port=5432 dbname=bloodbank user=requester password=requester");
+
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+
+	$sql = <<<EOF
+      SELECT * FROM users where username = '$username' and password = '$password';
+EOF;
+//	$query = "SELECT * FROM users where username = '$username' and password = '$password'"; 
+	$res = pg_query($db, $sql);
+
+	if(!$res){
+		echo pg_last_error($db);
+   	}
+
+   	$rows = pg_num_rows($res);
+	if (pg_num_rows($res)>0){
+		//the user is an admin
+		header("Location: admin-homepage.html");
+		exit();
+	}else{
+		//not an admin
+		header("Location: admin-login-error.html");
+		exit();
+	}
+?>
