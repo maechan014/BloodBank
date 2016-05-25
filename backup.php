22f73c932e97a73647151495eebccccfad134f20
Charie Mae <maechan014@gmail.com>
2016-05-25 10:28:21 +0800

    dump file and syntax

---
+++ backup.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>  
+<head> 
+<title>Back Up Database</title>  
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />  
+<style>  
+li {listt-style: none;}  
+</style>  
+</head>  
+<body> 	
+<h2>BACK UP DATABASE</h2>
+
+<ul>
+<form name="insert" action="add_donation.php" method="POST" >
+
+<input type="submit" name = "Submit"
+ value = "<?php echo htmlspecialchars($_GET['brh']); ?>"/>
+</form>  
+</ul>  
+</body>  
+</html>
